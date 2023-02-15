1e1908c4b22d95238d374244ea66d2288d5c6082
Manol Trendafilov <manol@passion4.tech>
2023-02-15 19:42:07 +0200

    add namespace for exception, define lines

--- src/TransactionData.php
+++ src/TransactionData.php
@@ -2,12 +2,12 @@
 namespace App;
 class TransactionData {
     private string $filePath;
-    private $lines = [];
+    private array $lines = [];
 
     public function __construct($filePath) {
 
         if (empty($filePath)) {
-            throw new Exception("Please pass the input file as an argument");
+            throw new \Exception("Please pass the input file as an argument");
         }
 
         $this->filePath = $filePath;
@@ -17,15 +17,18 @@ class TransactionData {
     public function read() {
         $fp = fopen($this->filePath, "r");
         if (!$fp) {
-            throw new Exception('The requested file could not be found!');
+            throw new \Exception('The requested file could not be found!');
         }
 
         while(!feof($fp)) {
             $this->lines[] = fgets($fp);
         }
+
+        fclose($fp);
     }
 
-    public function getLines() {
+    public function getLines(): array
+    {
         return $this->lines;
     }
 }
